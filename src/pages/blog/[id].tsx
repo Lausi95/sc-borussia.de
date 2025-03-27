bfddc1c4940124c000b5ebfbe3d4513ec1294d52
yuriypyrih <yuriypyrih@gmail.com>
2025-03-27 20:15:05 +0100

    feature(/blogs/[id]): new blogs details page

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,53 @@
+import { getDataAccess } from "@/datamodel/data-access";
+import Page, { PageProps } from "@/templates/page";
+import { BlogEntry } from "@/datamodel/model";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+
+type BlogDetailProps = PageProps & {
+  blogEntry: BlogEntry;
+};
+
+export default function BlogDetail({ menu, blogEntry }: BlogDetailProps) {
+  return (
+    <Page menu={menu} title={blogEntry.title}>
+      <article>
+        <div className="bg-[url(/default-blog-image.webp)] rounded-t bg-cover bg-center h-40"></div>
+        <h1 className="text-3xl font-bold mb-4 mt-6">{blogEntry.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: blogEntry.content }} />
+      </article>
+    </Page>
+  );
+}
+
+type Params = { id: string };
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  const { id } = context.params as Params;
+  const dataAccess = getDataAccess();
+
+  return {
+    props: {
+      menu: await dataAccess.getMenu(),
+      blogEntry: await dataAccess.getBlogEntry(id),
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const dataAccess = getDataAccess();
+  const blogEntries = await dataAccess.getBlogEntries();
+
+  const paths = blogEntries.map((entry) => {
+    // Remove '/blog/' prefix if it's there
+    const cleanId = entry.url.replace(/^\/blog\//, "");
+
+    return {
+      params: { id: cleanId },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
